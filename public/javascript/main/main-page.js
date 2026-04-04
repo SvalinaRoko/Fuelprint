bd9653b2490374782bfca6172950e99561151e11
SvalinaRoko <roko.svalina@gmail.com>
2026-04-04 14:40:34 +0200

    Dasboard-dev - fixed sidebar closing and opening issue

--- public/javascript/main/main-page.js
+++ public/javascript/main/main-page.js
@@ -1,39 +1,36 @@
 // HERO SECTION
+const sidebarContainer = document.querySelector(".sidebar-section");
+const sidebar = document.querySelector(".sidebar-content");
 function showButton(){
     const isMobile = window.matchMedia("(max-width:1280px)").matches;
     const brgerBtn = document.querySelector(".buttonMobile");
-    const sidebar = document.querySelector(".sidebar-content");
 
     if(!brgerBtn){
         return;
     }
     if(isMobile){
-        
+        sidebar.classList.add("closeSidebar");
+        sidebar.classList.remove("openSidebar");
+
         brgerBtn.classList.remove("hide");
         brgerBtn.classList.add("show");
         
-        sidebar.classList.remove("openSidebar");
-        sidebar.classList.add("closeSidebar");
     }else {
+        sidebar.classList.remove("closeSidebar");
+        sidebar.classList.remove("openSidebar");
 
         brgerBtn.classList.remove("show");
         brgerBtn.classList.add("hide");
 
-        sidebar.classList.remove("openSidebar");
-        sidebar.classList.remove("closeSidebar");
-
     }
 }
 showButton();
-
 window.addEventListener("resize", function() {
     showButton();
 })
 
 function enableSidebar(){
     const mobileBtn = document.querySelector(".buttonMobile");
-    const sidebarContainer = document.querySelector(".sidebar-section");
-    const sidebar = document.querySelector(".sidebar-content");
 
     function isOpened() {
         return mobileBtn.getAttribute("aria-expanded") === "true";
@@ -53,7 +50,7 @@ function enableSidebar(){
         
         document.body.classList.remove("no-scroll");
     }
-    
+
     function openSidebar(){
         mobileBtn.setAttribute("aria-expanded", true);
 
@@ -79,20 +76,16 @@ function enableSidebar(){
 
     window.addEventListener("resize", () => {
         if (window.innerWidth > 1280) {
-        openSidebar();
-        } else{
-        closeSidebar();
+            document.addEventListener('click', (e) => {
+                if(!isOpened()) return;
+                if(sidebar.contains(e.target) || mobileBtn.contains(e.target)){
+                    return;
+                }
+                closeSidebar();
+            })
         }
     })
 
-    document.addEventListener('click', (e) => {
-        if(!isOpened()) return;
 
-        if(sidebar.contains(e.target) || mobileBtn.contains(e.target)){
-            return;
-        }
-        closeSidebar();
-
-    })
 }
 enableSidebar();
